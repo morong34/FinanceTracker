f65ac045ae8b22fa14c3243c0510d6e508c49ae1
morong34 <morong_pereg@yahoo.com>
2022-01-31 21:51:15 +0200

    Added bootstrap 🙈

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
-import 'jquery-ujs';
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import "bootstrap" 
 
 Rails.start()
 Turbolinks.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  devise_for :users
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
